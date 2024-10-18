7c80201c1475bf476d04947923aba2efe163114f
BatteryRip <turbochel1000@gmail.com>
2024-10-18 14:31:29 +0300

    q

--- README.md
+++ README.md
@@ -1,16 +1,90 @@
-# untitled
+# translator_plus
+Free Google Translate API for Dart
 
-A new Flutter project.
+See it in pub: https://pub.dev/packages/translator_plus \
+and GitHub: https://github.com/HusamAwad2001/translator_plus
 
-## Getting Started
+# Usage
 
-This project is a starting point for a Flutter application.
+```dart
+void main() async {
+  final translator = GoogleTranslator();
 
-A few resources to get you started if this is your first Flutter project:
+  final input = "Здравствуйте. Ты в порядке?";
 
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+  translator.translate(input, from: 'ru', to: 'en').then(print);
+  // prints Hello. Are you okay?
+  
+  var translation = await translator.translate("Dart is very cool!", to: 'pl');
+  print(translation);
+  // prints Dart jest bardzo fajny!
 
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+  print(await "example".translate(to: 'pt'));
+  // prints exemplo
+}
+```
+&nbsp;
+
+Using **translate** method passing the args **from** and **to** designates the
+language from text you're typing and the language to be translated
+``` dart
+translator.translate("I love Brazil!", from: 'en', to: 'pt').then((s) {
+    print(s);
+  }); 
+  // prints Eu amo o Brasil!
+```
+&nbsp;
+
+or you can omit from language and it'll auto-detect the language of source text
+
+```dart
+translator.translate("Hello", to: 'es').then(print);
+// prints Hola
+```
+&nbsp;
+
+and also pass the value to a var using ```await```
+```dart
+var translation = await translator.translate("I would buy a car, if I had money.", from: 'en', to: 'it');
+print(translation);
+// prints Vorrei comprare una macchina, se avessi i soldi.
+```
+&nbsp;
+
+The returned value is a ```Translation``` object which holds the translation stuff
+```dart
+var translation = await translator.translate('Translation', from: 'en', to: 'es');
+print('${translation.source} (${translation.sourceLanguage}) == ${translation.text} (${translation.targetLanguage})');
+
+// prints Translation (English) == Traducción (Spanish)
+```
+&nbsp;
+
+You can use the ***extension method*** directly on the string too
+```dart
+print(await "example".translate(to: 'pt'));
+// prints exemplo
+```
+&nbsp;
+
+There is **translateAndPrint** method that prints directly
+```dart
+translator.translateAndPrint("This means 'testing' in chinese", to: 'zh-cn');
+// prints 这意味着用中文'测试'
+```
+&nbsp;
+
+# API
+For full API docs take a look at https://pub.dev/documentation/translator_plus/latest/
+
+
+# License
+MIT License
+
+Copyright © 2023 Husam Dahliz
+
+
+# Disclaimer
+This package is developed for educational purposes only. Do not depend on this package as it may break anytime as it is based on crawling the Google Translate website. Consider buying Official Google Translate API for other types of usage.
+
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:translator_plus/src/google_translator.dart';
 
 void main() {
+  final translator = GoogleTranslator();
   runApp(const MyApp());
 }
 
@@ -11,26 +13,88 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Translate',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Translate'),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
   final String title;
 
   @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final GoogleTranslator translator = GoogleTranslator();
+  final TextEditingController _textController = TextEditingController();
+  String _translatedText = '';
+
+  void _translateToEnglish() async {
+    final translation = await translator.translate(_textController.text, from: 'ru', to: 'en');
+    setState(() {
+      _translatedText = translation.text;
+    });
+  }
+
+  void _translateToRussian() async {
+    final translation = await translator.translate(_textController.text, from: 'en', to: 'ru');
+    setState(() {
+      _translatedText = translation.text;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('blablabla'),
+        title: Text(widget.title),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: _textController,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Введите текст',
+              ),
+              maxLines: 5,
+            ),
+            const SizedBox(height: 16),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                ElevatedButton(
+                  onPressed: _translateToEnglish,
+                  child: const Text('Перевести на английский'),
+                ),
+                ElevatedButton(
+                  onPressed: _translateToRussian,
+                  child: const Text('Перевести на русский'),
+                ),
+              ],
+            ),
+            const SizedBox(height: 16),
+            TextField(
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Результат перевода',
+              ),
+              controller: TextEditingController(text: _translatedText),
+              readOnly: true,
+              maxLines: 5,
+            ),
+          ],
+        ),
       ),
     );
   }
